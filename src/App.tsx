5415cc61d84c17847eb9aa604275b322b93da2f1
ellenassis <ellenassis8@gmail.com>
2021-09-22 18:04:46 -0300

    change files

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@
 import { Switch } from 'react-router-dom';
 
 import { BrowserRouter, Route } from 'react-router-dom';
-import { Home } from './pages/Home';
+import { Home } from './pages/Home/index';
 
-import { NewRoom } from "./pages/NewRoom";
-import { Room } from './pages/Room'
+import { NewRoom } from "./pages/NewRoom/index";
+import { Room } from './pages/Room/index'
 import { AuthContextProvider } from './contexts/AuthContext';
 
 
--- src/pages/Room.tsx
+++
@@ -1,7 +0,0 @@
-export function Room() {
-    return (
-        <div>
-            <h1>Oi</h1>
-        </div>
-    );
-}
